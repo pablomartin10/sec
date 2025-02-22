6bc37244f876dd7b553aaa805c91bc839b715946
pablomartin10 <63541552+pablomartin10@users.noreply.github.com>
2025-02-22 18:30:04 +0100

    Configure Vite project settings: set root directory, public assets folder, and build output directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()]
+  plugins: [react()],
+  root: './',  // Define la raíz del proyecto como la carpeta actual
+  publicDir: 'public', // Indica a Vite que busque los archivos estáticos en la carpeta 'public'
+  build: {
+    outDir: 'dist', // Directorio de salida de la compilación
+  },
 });
+// Archivo generado automáticamente
