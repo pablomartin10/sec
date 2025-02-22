8eb154037dc58c17e9f989b9f868a44adc47ef33
pablomartin10 <63541552+pablomartin10@users.noreply.github.com>
2025-02-22 18:32:02 +0100

    No code changes made

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-// Archivo generado automáticamente
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  // Define la raíz del proyecto como la carpeta actual
-  publicDir: 'public', // Indica a Vite que busque los archivos estáticos en la carpeta 'public'
   build: {
-    outDir: 'dist', // Directorio de salida de la compilación
+    outDir: 'dist',
   },
 });
+
 // Archivo generado automáticamente
